93067f8b201e1622a7d4189696bd108726e0a22f
jeanpaulino.ssp <jean.ssparaiso@gmail.com>
2023-03-03 11:26:44 -0300

    Criação da interface e normalização dos dados da API

--- src/script.ts
+++ src/script.ts
@@ -1,34 +1,13 @@
 import fetchData from "./fetchData.js";
-
-type TransacaoPagamento = "Boleto" | "Cartão de Crédito";
-type TransacaoStatus =
-  | "Paga"
-  | "Recusada pela operadora de cartão"
-  | "Aguardando pagamento"
-  | "Estornada";
-
-interface TransacaoAPI {
-  Nome: string;
-  ID: number;
-  Data: string;
-  Status: string;
-  Email: string;
-  ["Valor (R$)"]: string;
-  ["Cliente Novo"]: number;
-  ["Forma de Pagamento"]: TransacaoPagamento;
-}
+import normalizarTransacao from "./normalizarTransacao.js";
 
 async function handleData() {
   const data = await fetchData<TransacaoAPI[]>(
     "https://api.origamid.dev/json/transacoes.json"
   );
-  if (data) {
-    data.forEach((item) => {
-      console.log(item);
-    });
-  }
-
-  console.log("codigo continuou");
+  if (!data) return;
+  const transacoes = data.map(normalizarTransacao);
+  console.log(transacoes);
 }
 
 handleData();

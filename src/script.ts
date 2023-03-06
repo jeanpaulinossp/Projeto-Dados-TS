fb0721b404dca2a27fab5a6a4ef476de4ca2e296
jeanpaulino.ssp <jean.ssparaiso@gmail.com>
2023-03-06 11:30:46 -0300

    Criação da Estatística e função que soma o valor total das transações

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,4 @@
+import Estatisticas from "./Estatisticas.js";
 import fetchData from "./fetchData.js";
 import normalizarTransacao from "./normalizarTransacao.js";
 
@@ -8,6 +9,19 @@ async function handleData() {
   if (!data) return;
   const transacoes = data.map(normalizarTransacao);
   preencherTabela(transacoes);
+  preencherEstatisticas(transacoes);
+}
+
+function preencherEstatisticas(transacoes: Transacao[]): void {
+  const data = new Estatisticas(transacoes);
+  const totalElement = document.querySelector<HTMLElement>("#total span");
+  if (totalElement) {
+    totalElement.innerText = data.total.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+  }
+  console.log(data.total);
 }
 
 function preencherTabela(transacoes: Transacao[]): void {
